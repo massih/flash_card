658174a66b6d17f7ee5d0613147ef52ea4d41a65
Massih <massih.z@gmail.com>
2019-05-22 23:17:01 +0200

    Add getAll method, refactor entity

--- databasehandler.py
+++ databasehandler.py
@@ -1,23 +1,26 @@
-from sqlalchemy import create_engine, Column, Integer, Sequence, String
+from datetime import datetime
+
+from sqlalchemy import create_engine, Column, Integer, Sequence, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 
-class Vocab(Base):
-    __tablename__ = 'vocab'
+class Card(Base):
+    __tablename__ = 'cards'
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
-    word_swd = Column(String(250), nullable=False)
-    word_eng = Column(String(250), nullable=False)
+    word_original = Column(String(250), nullable=False)
+    word_meaning = Column(String(250), nullable=False)
     counter = Column(Integer, nullable=False)
     counter_incorrect = Column(Integer, nullable=False)
+    last_visit = Column(DateTime, nullable=False)
 
     def __repr__(self) -> str:
         return self.__dict__.__str__()
 
 
-class DataBase:
+class DataBaseHandler:
     DB_FILE = 'vocab.db'
 
     def __init__(self, file_name=DB_FILE):
@@ -25,7 +28,12 @@ class DataBase:
         Base.metadata.create_all(engine)
         self.session = sessionmaker(bind=engine)
 
-    def add_vocab(self, vocab_entity):
+    def add_vocab(self, card_entity: Card):
         session = self.session()
-        session.add(vocab_entity)
+        card_entity.last_visit = datetime.now()
+        session.add(card_entity)
         session.commit()
+
+    def get_all_words(self):
+        session = self.session()
+        return session.query(Card).order_by(Card.last_visit.desc()).all()
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from database import DataBase, Vocab
+from databasehandler import DataBaseHandler, Card
 
 
 def main():
-    db = DataBase()
-    vo = Vocab(word_swd='hej', word_eng='hello', counter=0, counter_incorrect=0)
+    db = DataBaseHandler()
+    vo = Card(word_original='hej', word_meaning='hello', counter=0, counter_incorrect=0)
     db.add_vocab(vo)
+    all_words = db.get_all_words()
+    [print(word) for word in all_words]
 
 
 if __name__ == '__main__':
