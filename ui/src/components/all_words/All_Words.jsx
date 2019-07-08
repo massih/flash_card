ef5bec673d3951537113fd1c1e35bbf715f33458
Massih <massih.z@gmail.com>
2019-07-09 00:45:44 +0200

    Add notification for success and error in newWord and allWord

--- ui/src/components/all_words/All_Words.jsx
+++ ui/src/components/all_words/All_Words.jsx
@@ -15,14 +15,14 @@ class AllWords extends Component {
       method: 'GET',
       headers: {'Content-Type': 'application/json'}
     }).then(res => res.json())
-      .then(response => {
-        let tableData = {};
-        response.forEach((row) => {
-          tableData[row.id] = row;
-        });
-        this.setState({'tableData': tableData});
-      })
-      .catch(error => console.log('Error:', error));
+        .then(response => {
+          let tableData = {};
+          response.forEach((row) => {
+            tableData[row.id] = row;
+          });
+          this.setState({'tableData': tableData});
+        })
+        .catch(error => console.log('Error:', error));
   }
 
   editButtonOnclick(rowId) {
@@ -38,7 +38,6 @@ class AllWords extends Component {
   }
 
   deleteButtonOnclick(rowId) {
-    console.log('delete button clicked for id => ', rowId);
     const url = 'http://localhost:8000/api/card/' + rowId;
     fetch(url, {
       method: 'DELETE',
@@ -46,11 +45,36 @@ class AllWords extends Component {
     }).then(() => {
       let tableData = this.state.tableData;
       delete tableData[rowId];
-      this.setState({'tableData': tableData})
-    })
-      .catch(error => console.log('Error:', error));
+      this.setState({'tableData': tableData});
+      this.addNotification('delete', 'success', 'Gone!')
+    }).catch(error => {
+      console.log('Error:', error);
+      this.addNotification('delete', 'danger', error.toString());
+    });
   }
 
+  addNotification(operation, type, message) {
+    let properties = AllWords.getNotificationProperties(operation, type, message);
+    this.props.notificationRef.current.addNotification({
+      title: properties.title,
+      message: message,
+      type: properties.type,
+      insert: "top",
+      container: "bottom-center",
+      animationIn: ["animated", "fadeIn"],
+      animationOut: ["animated", "fadeOut"],
+      dismiss: {duration: 5000},
+      dismissable: {click: true}
+    });
+  }
+
+  static getNotificationProperties(operation, type, message) {
+    return {
+      title: ((type === 'success') ? 'Successfully ' : 'Failed to ') + operation,
+      type: type,
+      message: message
+    };
+  }
 
   render() {
     if (!this.state.tableData) {
@@ -59,37 +83,38 @@ class AllWords extends Component {
 
     if (!Object.keys(this.state.tableData).length) {
       return (
-        <div className="section">
-          <p>You haven't add any words yet! Click on "New Word" to start!</p>
-        </div>
+          <div className="section">
+            <h3>You haven't add any words yet!</h3>
+            <p>Click on "New Word" to start!</p>
+          </div>
       );
     }
 
     return (
-      <div className="blue-grey darken-2">
-        <table className="highlight centered responsive-table">
-          <thead>
-          <tr>
-            <th>Word</th>
-            <th>Meaning</th>
-            <th>Actions</th>
-          </tr>
-          </thead>
-          <tbody>
-          {Object.keys(this.state.tableData).map((id, index) => (
+        <div className="blue-grey darken-2">
+          <table className="highlight centered responsive-table">
+            <thead>
             <tr>
-              <td>{this.state.tableData[id].word_original}</td>
-              <td>{this.state.tableData[id].word_meaning}</td>
-              <td>
-                <Button color="transparent" icon="edit" onClick={() => this.editButtonOnclick(id)}/>
-                <Button color="transparent" icon="delete" onClick={() => this.deleteButtonOnclick(id)}/>
-              </td>
+              <th>Word</th>
+              <th>Meaning</th>
+              <th>Actions</th>
             </tr>
-          ))
-          }
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+            {Object.keys(this.state.tableData).map((id, index) => (
+                <tr>
+                  <td>{this.state.tableData[id].word_original}</td>
+                  <td>{this.state.tableData[id].word_meaning}</td>
+                  <td>
+                    <Button color="transparent" icon="edit" onClick={() => this.editButtonOnclick(id)}/>
+                    <Button color="transparent" icon="delete" onClick={() => this.deleteButtonOnclick(id)}/>
+                  </td>
+                </tr>
+            ))
+            }
+            </tbody>
+          </table>
+        </div>
     );
   }
 }
