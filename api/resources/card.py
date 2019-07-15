43aafe8e181f1898c22b4080d4b4113c60698da7
Massih <massih.z@gmail.com>
2019-07-15 13:50:57 +0200

    WIP: Update flash card after User resposnse

--- api/resources/card.py
+++ api/resources/card.py
@@ -38,30 +38,3 @@ class CardResource:
         self.logger.info(f'received DELETE call for {card_id}')
         self.db.delete_card(card_id)
         resp.status = falcon.HTTP_200
-
-
-class CardsResources:
-    def __init__(self, db: DataBaseHandler):
-        self.db = db
-        self.logger = logging.getLogger(__name__)
-
-    def on_get(self, req, resp):
-        self.logger.info('received GET call')
-        all_cards = self.db.get_all_cards()
-        response = []
-        for card in all_cards:
-            response.append(card.to_json())
-        self.logger.info(all_cards)
-        resp.media = response
-
-    @validate(load_schema('card_creation'))
-    def on_post(self, req, resp):
-        self.logger.info('received POST call')
-        data = req.media
-        card = Card(
-            word_original=data.get('word_original'),
-            word_meaning=data.get('word_meaning')
-        )
-        self.db.add_card(card)
-        resp.status = falcon.HTTP_201
-        resp.media = card.to_json()
--- ui/src/components/flash_card/Flash_Card.jsx
+++ ui/src/components/flash_card/Flash_Card.jsx
@@ -6,10 +6,30 @@ class FlashCard extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
+      flash_card: {},
       meaning_user_input: '',
       result_icon: null,
       show_answer: false
     };
+    this.get_flash_card_data();
+  }
+
+  get_flash_card_data() {
+    if (this.props.flash_card) {
+      this.setState({flash_card: this.props.flash_card});
+      return;
+    }
+
+    let url = 'http://localhost:8000/api/flashcard';
+    fetch(url, {
+      method: 'GET',
+      headers: {'Content-Type': 'application/json'}
+    }).then(res => res.json())
+      .then(response => {
+        console.log('response: {}', response);
+        this.setState({flash_card: response});
+      })
+      .catch(error => console.log('Error:', error));
   }
 
   revealAnswer() {
@@ -17,7 +37,11 @@ class FlashCard extends Component {
   }
 
   buttonOnclick = () => {
-    const result = this.state.meaning_user_input.toLowerCase() === this.props.word_meaning.toLowerCase();
+    let flash_card = this.state.flash_card;
+    flash_card.counter += 1;
+    let result = this.state.meaning_user_input.toLowerCase() === flash_card.word_meaning.toLowerCase();
+
+    flash_card.counter_incorrect = (result) ? flash_card.counter_incorrect : flash_card.counter_incorrect +1;
     const icon = <i className="material-icons">{(result) ? 'thumb_up' : 'thumb_down'}</i>;
 
     this.setState({show_answer: true});
@@ -29,24 +53,25 @@ class FlashCard extends Component {
   };
 
   render() {
+    let flash_card = this.state.flash_card;
     return (
-        <div className="card cyan darken-4 large">
-          <div className="card-content white-text">
-            <h1>{this.props.word_original}</h1>
-            <TextField
-              id='meaning_user_input'
-              value={this.state.word_meaning}
-              onChange={this.textfieldOnchange}
-              label='Word Meaning'
-              large
-            />
-            <p>{this.state.result_icon}</p>
-            <h4 className={this.revealAnswer()}>The answer : {this.props.word_meaning}</h4>
-          </div>
-          <div className="card-action">
-            <Button onClick={this.buttonOnclick} icon="done" text="Done" color="green darken-2" large/>
-          </div>
+      <div className="card cyan darken-4 large">
+        <div className="card-content white-text">
+          <h1>{flash_card.word_original}</h1>
+          <TextField
+            id='meaning_user_input'
+            value={this.state.meaning_user_input}
+            onChange={this.textfieldOnchange}
+            label='Word Meaning'
+            large
+          />
+          <p>{this.state.result_icon}</p>
+          <h4 className={this.revealAnswer()}>The answer : {flash_card.word_meaning}</h4>
         </div>
+        <div className="card-action">
+          <Button onClick={this.buttonOnclick} icon="done" text="Done" color="green darken-2" large/>
+        </div>
+      </div>
     )
   }
 }
